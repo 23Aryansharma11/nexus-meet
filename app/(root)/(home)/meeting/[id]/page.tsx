3dfa9665a4e7376b5c86ed94620ded2a81c820ce
Aryan Sharma <23aryansharma11@gmail.com>
2024-04-12 20:10:51 +0530

    add auth, half done with homepage

---
+++ app/(root)/(home)/meeting/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface ID{
+    params: {
+        id: string
+    }
+}
+const Meeting = ({params}: ID) => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className=' text-3xl font-bold  '>
+        Meeting
+      </h1>
+    </section>
+  )
+}
+
+export default Meeting

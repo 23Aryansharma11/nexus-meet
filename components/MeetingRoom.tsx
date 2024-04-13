3cfe2fff71c5457c3eb6d6c8a9072e91557142ec
Aryan Sharma <23aryansharma11@gmail.com>
2024-04-13 19:31:51 +0530

    fix: fixed bug

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -6,12 +6,11 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users2Icon } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
 import Loader from './Loader'
 
@@ -40,6 +39,7 @@ const MeetingRoom = () => {
           return <SpeakerLayout participantsBarPosition="top" />
     }
   }
+  const router = useRouter()
   return (
     <section className='relative h-screen w-full overflow-hidden pt-4 text-white '>
       <div className=' relative flex size-full items-center justify-center'>
@@ -50,9 +50,8 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={()=> setshowParticipants(false)}/>
         </div>
       </div>
-
       <div className=' flex-wrap fixed bottom-0 flex w-full items-center justify-center gap-5'>
-          <CallControls />
+          <CallControls onLeave={()=> router.push('/')}/>
           <DropdownMenu>
             <div className=' flex items-center '>
             <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232D] px-4 py-2 hover:bg-[#4C535B]'> <LayoutList size={20} className=' text-white' /></DropdownMenuTrigger>
